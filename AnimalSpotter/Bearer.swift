e8bee01a15d4c6bb5a6b4349cd0f2d7041b1481e
Chris <chris.gonzales.82@gmail.com>
2020-02-13 08:47:17 -0800

    Updated project

---
+++ AnimalSpotter/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by Chris Gonzales on 2/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Bearer: Decodable{
+    let token: String
+}
---
+++ AnimalSpotter/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  AnimalSpotter
+//
+//  Created by Chris Gonzales on 2/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    
+    let username: String
+    let password: String
+}
